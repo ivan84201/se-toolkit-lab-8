692e996966a61f8a53e14fdbf6423f5f88d0fbf6
ivan84201 <i.sheluntcov@innopolis.university>
2026-04-01 23:17:36 +0300

    123

--- backend/src/lms_backend/routers/items.py
+++ backend/src/lms_backend/routers/items.py
@@ -25,8 +25,8 @@ async def get_items(session: AsyncSession = Depends(get_session)):
             extra={"event": "items_list_failed_as_not_found"},
         )
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Items not found",
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,                                                                     
+            detail="Internal server error",  
         ) from exc
 
 
